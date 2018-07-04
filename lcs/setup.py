b42a7688feaf68d625b5d5dbbaca35f7d30ef25c
Matias Lindgren <matias.lindgren@aalto.fi>
2018-07-04 13:04:24 +0300

    Add trivial cpu-heavy Python library

---
+++ lcs/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lcs',
+    version='0.1.0',
+    description='Naive longest common substring solver',
+    packages=find_packages(),
+)
