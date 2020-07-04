fca3f037a45557a1c26cedd38f63d368d4d120a4
Matias Lindgren <matias.lindgren@gmail.com>
2020-07-04 23:44:36 +0300

    add deployments for celery workers and msg broker

---
+++ consumer-large/celery_app.py
@@ -0,0 +1,14 @@
+"""
+CLI Celery app for handling lcs.tasks.longest_common_substr calls asynchronously.
+"""
+import os
+from celery import Celery
+from lcs.tasks import longest_common_substr
+
+def make_celery():
+    return Celery("consumer-large",
+            backend=os.environ.get("CELERY_RESULT_BACKEND"),
+            broker=os.environ["CELERY_BROKER_URL"])
+
+if __name__ == "__main__":
+    make_celery().worker_main()
